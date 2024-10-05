8507c90d80784c5c958d775892079237444b4c29
3umaGH <3uma+github@protonmail.com>
2024-10-05 15:10:43 +0300

    feat: implement progress bar

---
+++ src/components/common/ProgressBar.tsx
@@ -0,0 +1,17 @@
+import clsx from 'clsx'
+import { CommonProps } from '../../types/common'
+
+type ProgressBar = {
+  value: number
+  maxValue: number
+} & CommonProps
+
+export const ProgressBar = ({ value, maxValue, className }: ProgressBar) => {
+  const filledPercent = maxValue === 0 ? 0 : (value / maxValue) * 100
+
+  return (
+    <div className={clsx('relative w-full overflow-hidden h-[30px] bg-gray-400 rounded-xl', className)}>
+      <div className='absolute top-0 left-0 bg-[--primary] h-full' style={{ minWidth: `${filledPercent}%` }}></div>
+    </div>
+  )
+}
