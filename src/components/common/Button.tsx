c2ad9c7c5697fce83a565b006bd843f60f192599
3umaGH <3uma+github@protonmail.com>
2024-10-05 15:42:20 +0300

    feat: implement button component

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx'
+import React from 'react'
+import { CommonProps } from '../../types/common'
+
+type Button = { children: React.ReactNode; disabled: boolean } & React.ButtonHTMLAttributes<HTMLButtonElement> &
+  CommonProps
+
+export const Button = ({ className, children, disabled, ...props }: Button) => {
+  return (
+    <button
+      {...props}
+      disabled={disabled}
+      className={clsx(
+        'bg-[--primary] disabled:opacity-50 text-white rounded-xl text-base font-bold p-4 uppercase hover:brightness-90 transition-all w-full',
+        { 'active:scale-95': !disabled },
+        className
+      )}>
+      {children}
+    </button>
+  )
+}
