bc501d3b4cb964c1af03c4f1d0fac660b8dc66fc
3umaGH <3uma+github@protonmail.com>
2024-10-05 14:36:58 +0300

    fix: card style change on flip

--- src/components/pages/QuizPage/FlashCard.tsx
+++ src/components/pages/QuizPage/FlashCard.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx'
-import { useMemo } from 'react'
+import { memo, useEffect, useState } from 'react'
 import { QuestionGrade } from '../../../constants/quiz'
+import { FlashCardQuestion } from '../../../types/quiz'
 import { randomIntFromInterval } from '../../../util'
 
 const CARD_STYLES = [
@@ -19,18 +20,25 @@ const CARD_STYLES = [
   'bg-blue-500 text-white',
 ]
 
+const getRandomCardStyle = () => {
+  return CARD_STYLES[randomIntFromInterval(0, CARD_STYLES.length - 1)]
+}
+
 type FlashCard = {
-  question: string
-  answer: string
+  question: FlashCardQuestion
   flipped: boolean
   onCardClick: () => void
   onGradeClick: (grade: QuestionGrade) => void
 }
 
-export const FlashCard = ({ question, answer, flipped, onGradeClick, onCardClick }: FlashCard) => {
-  const cardClassname = useMemo(() => CARD_STYLES[randomIntFromInterval(0, CARD_STYLES.length - 1)], [])
+export const FlashCard = memo(({ question, flipped, onGradeClick, onCardClick }: FlashCard) => {
   const commonClassname =
     'border-4 outline outline-gray-500 p-4 text-center flex justify-center items-center rounded-md border-gray-400 drop-shadow-lg'
+  const [cardClassname, setCardClassname] = useState(getRandomCardStyle())
+
+  useEffect(() => {
+    setCardClassname(getRandomCardStyle())
+  }, [question.id])
 
   return (
     <div
@@ -41,39 +49,41 @@ export const FlashCard = ({ question, answer, flipped, onGradeClick, onCardClick
       onClick={onCardClick}>
       <div className='w-full card-inner'>
         <div className={clsx('card-front text-xl lg:text-2xl font-medium', commonClassname, cardClassname)}>
-          {question}
+          {question.question}
         </div>
         <div className={clsx('card-back', commonClassname, cardClassname)}>
           <div className='w-full h-full bg-black opacity-20 absolute top-0 left-0 z-[1]' />
 
-          {flipped &&  <div className='absolute z-[2] p-4 flex flex-col h-full justify-evenly'>
-            <div className='text-xl font-medium lg:text-2xl'>{question}</div>
-           <div className='text-lg font-medium lg:text-xl'>{answer}</div>
+          {flipped && (
+            <div className='absolute z-[2] p-4 flex flex-col h-full justify-evenly'>
+              <div className='text-xl font-medium lg:text-2xl'>{question.question}</div>
+              <div className='text-lg font-medium lg:text-xl'>{question.answer}</div>
 
-            <div className={clsx('flex flex-col items-center opacity-0', { 'animate-fade-in-slow': flipped })}>
-              <span>How close was your answer?</span>
+              <div className={clsx('flex flex-col items-center opacity-0', { 'animate-fade-in-slow': flipped })}>
+                <span>How close was your answer?</span>
 
-              <div className='flex justify-between mt-6 flex-wrap max-w-[500px] w-full px-8'>
-                <span className='answer-grading-button' onClick={() => onGradeClick(QuestionGrade.EXCELLENT)}>
-                  🌟
-                </span>
-                <span className='answer-grading-button' onClick={() => onGradeClick(QuestionGrade.GOOD)}>
-                  😊
-                </span>
-                <span className='answer-grading-button' onClick={() => onGradeClick(QuestionGrade.FAIR)}>
-                  😐
-                </span>
-                <span className='answer-grading-button' onClick={() => onGradeClick(QuestionGrade.POOR)}>
-                  😬
-                </span>
-                <span className='answer-grading-button' onClick={() => onGradeClick(QuestionGrade.VERY_POOR)}>
-                  💀
-                </span>
+                <div className='flex justify-between mt-6 flex-wrap max-w-[500px] w-full px-8'>
+                  <span className='answer-grading-button' onClick={() => onGradeClick(QuestionGrade.EXCELLENT)}>
+                    🌟
+                  </span>
+                  <span className='answer-grading-button' onClick={() => onGradeClick(QuestionGrade.GOOD)}>
+                    😊
+                  </span>
+                  <span className='answer-grading-button' onClick={() => onGradeClick(QuestionGrade.FAIR)}>
+                    😐
+                  </span>
+                  <span className='answer-grading-button' onClick={() => onGradeClick(QuestionGrade.POOR)}>
+                    😬
+                  </span>
+                  <span className='answer-grading-button' onClick={() => onGradeClick(QuestionGrade.VERY_POOR)}>
+                    💀
+                  </span>
+                </div>
               </div>
             </div>
-          </div>}
+          )}
         </div>
       </div>
     </div>
   )
-}
+})
