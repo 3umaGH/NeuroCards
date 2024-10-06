7e60f5aa4f293124dd2afc21f2f7a02ca6e4a545
3umaGH <3uma+github@protonmail.com>
2024-10-06 22:14:53 +0300

    style: small adjustments for smaller screens

--- src/pages/BrowseQuiz.tsx
+++ src/pages/BrowseQuiz.tsx
@@ -2,7 +2,7 @@ import { QuizTable } from '../components/pages/BrowseQuiz/QuizTable/QuizTable'
 
 export const BrowseQuiz = () => {
   return (
-    <div className='flex flex-col flex-1 w-full h-full gap-8 max-w-[1000px] items-center'>
+    <div className='flex flex-col flex-1 w-full h-full gap-4 md:gap-8 max-w-[1000px] items-center'>
       <h3 className='w-full text-xl font-bold text-white md:text-4xl'>Explore Available Quizzes</h3>
 
       <QuizTable />
