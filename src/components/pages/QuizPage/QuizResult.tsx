0a2411812349c726d15120e03de9d458e1ec556b
3umaGH <3uma+github@protonmail.com>
2024-10-07 17:46:46 +0300

    fix: result page flickering on quiz start

--- src/components/pages/QuizPage/QuizResult.tsx
+++ src/components/pages/QuizPage/QuizResult.tsx
@@ -42,7 +42,7 @@ export const QuizResult = ({
   }
 
   return (
-    <div className='flex flex-col items-center w-full h-full p-4 py-4 overflow-auto bg-white justify-evenly rounded-3xl'>
+    <div className='flex flex-col items-center w-full h-full p-4 py-4 overflow-auto bg-white animate-fade-in justify-evenly rounded-3xl'>
       <h1 className='my-8 text-3xl font-black uppercase md:my-16 md:text-5xl animate-bounce'>Complete!</h1>
 
       <div className='flex flex-col flex-1 w-full overflow-auto min-h-[150px] max-w-full border rounded-xl gap-2 p-2 md:p-4'>
