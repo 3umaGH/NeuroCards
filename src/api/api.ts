cb9df958e0ab20bd3b8cff08314489e8054ae5e6
3umaGH <3uma+github@protonmail.com>
2024-10-08 23:54:11 +0300

    feat: implement env vars

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { FlashCardQuiz, FlashCardQuizDraft, QuizTableItem } from '../types/quiz'
 import { ConfigDTO } from '../types/config'
 
-const BASE_URL = 'http://localhost:3000/v1'
+const BASE_URL = import.meta.env.VITE_APP_API_URL
 
 export const getConfig = async (): Promise<ConfigDTO> => {
   return (await axios.get(`${BASE_URL}/config`)).data
